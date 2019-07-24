74052d6050f99cae9add6d751b3b9f2f005507b5
Jaremy Creechley <creechley@gmail.com>
2019-07-24 12:00:25 -0600

    init

---
+++ lib/jitter_entropy_port.ex
@@ -0,0 +1,18 @@
+defmodule JitterEntropyPort do
+  @moduledoc """
+  Documentation for JitterEntropyPort.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JitterEntropyPort.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jitter_entropy_port_test.exs
@@ -0,0 +1,8 @@
+defmodule JitterEntropyPortTest do
+  use ExUnit.Case
+  doctest JitterEntropyPort
+
+  test "greets the world" do
+    assert JitterEntropyPort.hello() == :world
+  end
+end
