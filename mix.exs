5d2c47e3b1a9d890d024d3bc45236ad126514ed1
Jaremy Creechley <creechley@gmail.com>
2019-07-24 13:38:34 -0600

    adding package info

--- mix.exs
+++ mix.exs
@@ -8,7 +8,11 @@ defmodule JitterEntropyPort.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      compilers: [:elixir_make] ++ Mix.compilers()
+      description: description(),
+      package: package(),
+      compilers: [:elixir_make] ++ Mix.compilers(),
+      name: "JitterEntropyPort",
+      source_url: "https://github.com/elcritch/jitter_entropy_port"
     ]
   end
 
@@ -18,6 +22,10 @@ defmodule JitterEntropyPort.MixProject do
       mod: {JitterEntropyPort, []},
       applications: [:logger]
     ]
+  end
+
+  defp description() do
+    "Runs the jitterentropy-rngd using Mountrop to provide extra random noise from CPU jitter"
   end
 
   defp package do
