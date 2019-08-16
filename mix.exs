3cc197c4fb26049028d2d115ae596284f4db890b
Jaremy Creechley <creechley@gmail.com>
2019-08-16 11:57:06 -0600

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JitterEntropyPort.MixProject do
   def project do
     [
       app: :jitter_entropy_port,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
